46355b68754b7492a62301264b82246cd5d7a1e6
Niki5225 <nikolayradkov97.im@gmail.com>
2023-03-13 16:38:52 +0200

    py

---
+++ OOP/polymorphism and abstraction/shapes.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        return None
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        return None
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return pi * self.__radius ** 2
+
+    def calculate_perimeter(self):
+        return pi * self.__radius * 2
+
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_area(self):
+        return self.__height * self.__width
+
+    def calculate_perimeter(self):
+        return 2 * (self.__height + self.__width)
+
+
+rectangle = Rectangle(10, 20)
+print(rectangle.calculate_area())
+print(rectangle.calculate_perimeter())
+
