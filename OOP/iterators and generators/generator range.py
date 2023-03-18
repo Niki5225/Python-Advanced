6073785c5b6c2261a6393dc54af9d4df9549f197
Niki5225 <nikolayradkov97.im@gmail.com>
2023-03-18 17:20:22 +0200

    py

---
+++ OOP/iterators and generators/generator range.py
@@ -0,0 +1,4 @@
+def genrange(start_num, end_num):
+    while start_num <= end_num:
+        yield start_num
+        start_num += 1
---
+++ OOP/iterators and generators/vowels.py
@@ -0,0 +1,23 @@
+class vowels:
+    def __init__(self, string: str):
+        self.string = string
+        self.start_index = 0
+        self.end_index = len(string)
+        self.vowels = ('a', 'e', 'i', 'o', 'u')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.start_index < self.end_index:
+            output = self.string[self.start_index]
+            self.start_index += 1
+
+            if output.lower() in self.vowels:
+                return output
+        else:
+            raise StopIteration()
+
+my_string = vowels('Abcedifuty0o')
+for char in my_string:
+    print(char)
